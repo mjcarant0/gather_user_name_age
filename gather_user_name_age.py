4ecd2290e5894c2f8b1b7f29f88a2458e4be5f9d
mjcarant0 <marjoymcaranto@iskolarngbayan.pup.edu.ph>
2024-10-21 01:21:59 +0800

    Adding the '[name]' in user_data at line 11

--- gather_user_name_age.py
+++ gather_user_name_age.py
@@ -1,20 +1,20 @@
 #Use dictionary to store the user's data
 user_data = {}
 #Use loop to ask user to input their name and age
-while True: #firs loop
+while True: #first loop
     #Another loop if there is an error or if the user wants to retry
     while True: #second loop
         try:
             name = input("What's your name? ")
             age = int(input("How old are you? "))
             
-            user_data = {
+            user_data[name] = {
                 "name" : name,
                 "age" : age
             }
             
             #Print user's information
-            print("--------------------------")
+            print("----------------------------------")
             print(f"This is your personal information")
             print(f"Name: {user_data[name]['name']}")
             print(f"Age: {user_data[name]['age']}")
