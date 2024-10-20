3c25c110a09066b90cccbffaa080a308cce78e86
mjcarant0 <marjoymcaranto@iskolarngbayan.pup.edu.ph>
2024-10-21 01:00:06 +0800

    Adding a dictionary (array) and loop to ask user to input their name and age

--- gather_user_name_age.py
+++ gather_user_name_age.py
@@ -1,5 +1,10 @@
 #Use dictionary to store the user's data
-
+user_data = {}
 #Use loop to ask user to input their name and age
-
+while True:
+    #Another loop if there is an error or if the user wants to retry
+    while True:
+        try:
+            name = input("What's your name? ")
+            age = int(input("How old are you? "))
 #Print user's information
