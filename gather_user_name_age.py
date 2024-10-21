b4dd853e00e26a63ece219b518a74ba25d9010d0
mjcarant0 <marjoymcaranto@iskolarngbayan.pup.edu.ph>
2024-10-22 00:23:50 +0800

    Changing the min and max into items method and conditional/If statements to find the oldest and youngest user

--- gather_user_name_age.py
+++ gather_user_name_age.py
@@ -54,7 +54,35 @@ while True: #first loop
 #Print all the stored data in user_data
 print("----------------------------------")
 print("       All stored user data       ")
-for name, data in user_data.items():
+for name, data in user_data.items(): #for x, y in dictionary.items()
     print(f"Name: {data['name']}, Age: {data['age']}")
 
 #Compare all the ages and compare it to find the oldest and youngest
+print("----------------------------------")
+#variable for oldest user/s
+oldest_name = {} 
+oldest_age = None
+#variable for youngest user/s
+youngest_name = {} 
+youngest_age = None
+
+for name, data in user_data.items():
+    age = data['age']
+    
+    #Compare the age of the users to find the oldest user/s
+    if oldest_age is None or age > oldest_age:
+        oldest_age = age
+        oldest_name = {name: age}
+    elif age == oldest_age:
+        oldest_name[name] = age
+    
+    #Compare the age of the users to find the youngest user/s
+    if youngest_age is None or age < youngest_age:
+        youngest_age = age
+        youngest_name = {name: age}
+    elif age == youngest_age:
+        youngest_name[name] = age
+    
+if oldest_name and youngest_name:
+    print(f"The oldest user(s) is/are {', '.join(oldest_name)} with an age of {oldest_age}")
+    print(f"The youngest user(s) is/are {', '.join(youngest_name)} with an age of {youngest_age}")
